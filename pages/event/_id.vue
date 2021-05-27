2fab5b9a9e2d2e597f3aeda063b6b869c413218d
AlinaDev252 <costachealina25@gmail.com>
2021-05-27 15:13:37 +0300

    Add head property for SEO, to my dynamic route

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    <h1>Event #{{ this.$route.params.id }}</h1>
+    <h1>Event #{{ id }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  // property used by vue-meta
+  head() {
+    return {
+      title: "Event #" + this.id,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "What you need to know about event #" + this.id
+        }
+      ]
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.params.id;
+    }
+  }
+};
+</script>
